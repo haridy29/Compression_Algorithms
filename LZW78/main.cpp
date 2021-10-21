400567fa2abedbecb2bab2bb311b8f24dfc34635
haridy29 <76223501+haridy29@users.noreply.github.com>
2021-10-21 19:58:34 +0200

    Create main.cpp

---
+++ LZW78/main.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Tag {
+    int index;
+    char Next;
+
+    Tag(int idx, char Next) {
+        index = idx;
+        this->Next = Next;
+    }
+};
+
+vector<Tag> Tags;
+map<int,string>cmp;
+void LZ78(const string &s) {
+
+    int m = 1;
+    map<string, int> mp;
+    for (int i = 0; i < s.size(); ++i) {
+        string ss = "";
+
+        for (int j = i; j < s.size(); ++j) {
+            ss += s[j];
+
+            auto it = mp.find(ss);
+            if (it == mp.end()) {//not found
+                if (ss.size() == 1) {
+
+                    Tag t(0, ss[0]);
+                    Tags.push_back(t);
+                    mp[ss] = m++;
+                    cmp[m-1]=ss;
+                } else {
+                    string new_s = ss;
+                    mp[ss] = m++;
+                    cmp[m-1]=ss;
+                    new_s.pop_back();
+                    Tag t(mp[new_s], ss[ss.size() - 1]);
+                    Tags.push_back(t);
+                }
+                i = j;
+                break;
+            }
+            else {
+                if(j==s.size()-1){
+
+                    string new_s = ss;
+                    mp[ss] = m++;
+                    new_s.pop_back();
+                    Tag t(mp[new_s], ss[ss.size() - 1]);
+                    Tags.push_back(t);
+
+
+                }
+                continue;
+            }
+
+
+        }
+    }
+
+}
+
+void ExtractLZ78(){
+    for (int i = 0; i <Tags.size() ; ++i) {
+        cout<<cmp[Tags[i].index]<<Tags[i].Next;
+    }
+}
+int main() {
+    string s;
+    cin >> s;
+
+    LZ78(s);
+    ExtractLZ78();
+
+}
