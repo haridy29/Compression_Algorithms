59de6a3282a61452a6c5ce6b0483120da047db30
haridy29 <76223501+haridy29@users.noreply.github.com>
2021-10-28 01:51:04 +0200

    Update main.cpp

--- LZ_78/main.cpp
+++ LZ_78/main.cpp
@@ -13,7 +13,7 @@ struct Tag {
 };
 
 vector<Tag> Tags;
-map<int,string>cmp;
+
 void LZ78(const string &s) {
 
     int m = 1;
@@ -29,21 +29,22 @@ void LZ78(const string &s) {
 
                 string new_s = ss;
                 mp[ss] = m++;
-                cmp[m-1]=ss;
                 new_s.pop_back();
                 Tag t(mp[new_s], ss[ss.size() - 1]);
                 Tags.push_back(t);
 
                 i = j;
                 break;
-            }
-            else {
-                if(j==s.size()-1){
+            } else {
+                if (j == s.size() - 1) {
+
                     string new_s = ss;
                     mp[ss] = m++;
                     new_s.pop_back();
                     Tag t(mp[new_s], ss[ss.size() - 1]);
                     Tags.push_back(t);
+                    i = j;
+
                 }
                 continue;
             }
@@ -52,18 +53,61 @@ void LZ78(const string &s) {
         }
     }
 
-}
+};
 
-void ExtractLZ78(){
-    for (int i = 0; i <Tags.size() ; ++i) {
-        cout<<cmp[Tags[i].index]<<Tags[i].Next;
+void ExtractLZ78(fstream &out) {
+    int m = 1;
+    map<int, string> mp;
+    mp[0] = "";
+    for (int i = 0; i < Tags.size(); ++i) {
+
+        Tag tag = Tags[i];
+
+        if (i != Tags.size() - 1)
+            mp[m++] = mp[tag.index] + tag.Next;
+        out << mp[tag.index] + tag.Next;
     }
+
 }
+
 int main() {
-    string s;
-    cin >> s;
+    char s[1000000];
+    string ss = "";
+    fstream infile, out;
+    infile.open("Test.txt", ios::in);
+    out.open("output.txt", ios::out);
 
-    LZ78(s);
-    ExtractLZ78();
+    if (infile) {
+        infile.getline(s, 1000000);
 
+        while (infile) {
+            ss += s;
+            infile.getline(s, 1000000);
+            ss += '\n';
+        }
+        ss.pop_back();
+
+    } else {
+        cout << "unable to open file\n";
+    }
+
+    LZ78(ss);
+
+    ExtractLZ78(out);
+    int largeindex = 0;
+    for (int i = 0; i < Tags.size(); ++i) {
+        largeindex = max(largeindex, Tags[i].index);
+    }
+    cout << "===========================================================\n";
+    cout << setfill(' ') << setw(40) << "Welcome in LZ78\n";
+    cout << "===========================================================\n";
+    cout << "Original size = " << ss.size() * 8 << " Bits\n";
+
+    cout << "Compressed size = " << (ceil(log2(largeindex)) + 8) * Tags.size() << " Bits\n";
+
+    cout << "===========================================================\n";
+
+    Tags.clear();
+    out.close();
+    infile.close();
 }
