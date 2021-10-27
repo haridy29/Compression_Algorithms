26a4fccaf4f0a1dbe2104f444f5cc55c78e87d5c
haridy29 <76223501+haridy29@users.noreply.github.com>
2021-10-28 01:49:51 +0200

    Update main.cpp

--- LZ_78/main.cpp
+++ LZ_78/main.cpp
@@ -27,12 +27,12 @@ void LZ78(const string &s) {
             auto it = mp.find(ss);
             if (it == mp.end()) {//not found
 
-                    string new_s = ss;
-                    mp[ss] = m++;
-                    cmp[m-1]=ss;
-                    new_s.pop_back();
-                    Tag t(mp[new_s], ss[ss.size() - 1]);
-                    Tags.push_back(t);
+                string new_s = ss;
+                mp[ss] = m++;
+                cmp[m-1]=ss;
+                new_s.pop_back();
+                Tag t(mp[new_s], ss[ss.size() - 1]);
+                Tags.push_back(t);
 
                 i = j;
                 break;
